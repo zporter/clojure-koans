fc09e20d48392705a517b8642df526afaff4bf8a
Zachary Porter <zporter9@gmail.com>
2011-08-14 19:34:38 -0400

    solves recursion

--- src/koans/recursion.clj
+++ src/koans/recursion.clj
@@ -1,20 +1,28 @@
 (defn is-even? [n]
   (if (= n 0)
-      __
-      (___ (is-even? (dec n)))))
+      true
+      (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-        __
+        acc
         (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         acc  ()]
+    (if (seq coll)
+      (recur (rest coll) (conj acc (first coll)))
+      acc)))
 
 (defn factorial [n]
-  __)
+  (loop [n   n
+         acc 1]
+   (if (zero? n)
+       acc
+       (recur (dec n) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
